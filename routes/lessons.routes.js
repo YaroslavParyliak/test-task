2422780ea79ff77a5dff76c7fa84ed221da5fcbd
Yaroslav Paryliak <jaroslavpariljak@gmail.com>
2020-01-29 22:20:56 +0200

    Defined Routes

---
+++ routes/lessons.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+    const lessons = require("../controllers/lessons.controller.js");
+  
+    // Create a new Lesson
+    app.post("/lessons", lessons.create);
+  
+    // Retrieve all Lessons
+    app.get("/lessons", lessons.findAll);
+  
+    // Retrieve a single Lesson with lessonId
+    app.get("/lessons/:lessonId", lessons.findOne);
+  
+    // Update a Lesson with lessonid
+    app.put("/lessons/:lessonId", lessons.update);
+  
+    // Delete a Lesson with lessonid
+    app.delete("/lessons/:lessonId", lessons.delete);
+  
+    // Create a new Lesson
+    app.delete("/lessons", lessons.deleteAll);
+  };
